5260fd4b7f4fdf7151499ece7c9f1ac8ebf6d4b1
David Mackay <mackaydavid808@gmail.com>
2023-06-09 14:45:49 -0400

    Initial Commit

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='name_of_product',
+    name='gpt_code_review',
     version='1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'name_of_product = name_of_product.entry_points.main:main'
+            'gpt_review = gpt_code_review.entry_points.code_review:main'
         ]
     }
 )
